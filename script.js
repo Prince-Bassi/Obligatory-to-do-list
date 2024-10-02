c258c5d532d8020c393fb88960fc46101aeb351e
prince <plol13102005@gmail.com>
2024-10-02 23:07:38 +0530

    Added test screen changing and the design and checkmark animation for tasks

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+taskList = document.querySelector(".taskBody .taskList");
+emptyList = document.querySelector(".taskBody .empty");
+tasks = document.querySelectorAll(".taskBody .taskList .task");
 
+document.addEventListener("keydown", (event) => {
+       if (event.key == "k") {
+              taskList.classList.toggle("active");
+              emptyList.classList.toggle("active");
+       }
+});
+
+function taskToggle(elem) {
+       elem.classList.toggle("check");
+}
+
+tasks.forEach(elem => {
+       elem.addEventListener("click", () => {
+              taskToggle(elem);
+       });
+});
