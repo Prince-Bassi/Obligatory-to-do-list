3d4e09e1aac0b7e05b617923953180b07851fc39
prince <plol13102005@gmail.com>
2024-10-08 21:18:53 +0530

    Added a detail screen, its styles, changing behaviour to detail screen.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-taskList = document.querySelector(".taskBody .taskList");
-emptyList = document.querySelector(".taskBody .empty");
-tasks = document.querySelectorAll(".taskBody .taskList .task");
+const taskList = document.querySelector(".taskBody .taskList");
+const emptyList = document.querySelector(".taskBody .empty");
+const checkmarks = document.querySelectorAll(".taskBody .taskList .task .checkmark");
+const tasks = document.querySelectorAll(".taskBody .taskList .task");
+const taskDetails = document.querySelector(".taskBody .taskDetails");
+const taskDetailTitleText = document.querySelector(".taskBody .taskDetails .taskTitle .text");
+const taskDetailDescText = document.querySelector(".taskBody .taskDetails .taskDesc .text");
+const taskDetailDeadlineText = document.querySelector(".taskBody .taskDetails .deadline .text");
+let ACTIVE_SCREEN = taskList;
 
 document.addEventListener("keydown", (event) => {
        if (event.key == "k") {
-              taskList.classList.toggle("active");
-              emptyList.classList.toggle("active");
+              emptyList.classList.add("active");
+              ACTIVE_SCREEN.classList.remove("active");
        }
 });
 
-function taskToggle(elem) {
-       elem.classList.toggle("check");
+function taskToggle(task) {
+       task.classList.toggle("check");
 }
+
+function viewDetails(task) {
+       taskDetailTitleText.textContent = "Check";
+       taskDetailDescText.textContent = "Description";
+       taskDetailDeadlineText.textContent = "Moay";
+
+       taskDetails.classList.add("active");
+       ACTIVE_SCREEN.classList.remove("active");
+}
+
+checkmarks.forEach(elem => {
+       elem.addEventListener("click", () => {
+              taskToggle(elem.parentNode);
+       });
+});
 
 tasks.forEach(elem => {
        elem.addEventListener("click", () => {
-              taskToggle(elem);
+              viewDetails(elem);
        });
 });
